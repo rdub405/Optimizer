3aac8ab36ca0d738cbff778bd0cecde02b6205c0
Rashaw Williams <rashaww@gmail.com>
2017-10-02 14:37:06 -0500

    Update NBA Tweets.R

--- NBA Tweets.R
+++ NBA Tweets.R
@@ -2,10 +2,10 @@ library("twitteR")
 library("ROAuth")
 
 # Set API Keys
-api_key <- "DTEcxRmKgDpPOsZrFq6LVdOtc"
-api_secret <- "YSunJxmG4tQtktIwFfByDyNjkAmOEt14UcIjtu5YyUGaYbrK8a"
-access_token <- "17455353-yUpMBFl4wS1UQ6OhiENozkVHLdlr9Kz7i4sQKhLqd"
-access_token_secret <- "Sdtb9pz24EJFZbrCYsgp839gN1AOVmH1DL5nkLeiS2MOi"
+api_key <- "x"
+api_secret <- "x"
+access_token <- "x"
+access_token_secret <- "x"
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
 search.string <- "#nba"
